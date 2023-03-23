7cf22ebc43df32a729524832e6717414cf521ed9
LSeita <seita.lucca@gmail.com>
2023-03-23 13:40:50 -0300

    Adiciona operate() a script.js

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-function divide(num1, num2){
-    return num1/num2;
+
+function operate(num1, num2, operator){
+    switch(operator){
+        case "+":
+            return add(num1,num2);
+        case "-":
+            return subtract(num1,num2);
+        case "/":
+            return divide(num1,num2);
+        case "*":
+            return multiply(num1,num2);
+        default:
+            return "Not valid!"
+    }
 }
 
 
-function subtract(num1, num2){
-    return num1-num2;
-}
 
+let divide = (num1, num2) => num1/num2;
+let subtract = (num1, num2) => num1-num2;
+let multiply = (num1, num2) => num1 * num2;
+let add = (num1, num2) => num1 + num2;
 
-function multiply(num1, num2){
-    return num1 * num2;
-}
-
-
-function add(num1, num2){
-    return num1 + num2;
-}
