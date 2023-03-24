f39599b6b82f0f7571df61fd7ac9f66d648df0c2
LSeita <seita.lucca@gmail.com>
2023-03-24 16:17:37 -0300

    Adiciona funcoes de operacao aritmetica a script.js

--- script.js
+++ script.js
@@ -1,12 +1,71 @@
 const buttons = document.querySelectorAll(".button");
 const display = document.querySelector(".display");
-display.textContent = 0;
+let operand1, operand2, operator;
+let displayValue = 0;
+let opSigns = ["/", "*", "+", "-"];
+
+updateDisplay();
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
-        if(!isNaN(button.value)) updateDisplay(button.value)
+        if(button.classList.contains("number")){
+            if(!operand1){
+                displayValue == 0 ? displayValue = button.value 
+                                  : displayValue += button.value;
+                updateDisplay();
+            }else{
+                displayValue == operand1 ? displayValue = button.value
+                                         : displayValue += button.value;
+                updateDisplay();
+            }
+        }
+        if(button.classList.contains("operator")){
+            if(opSigns.includes(button.value)){
+                switch(!operand1){
+                    case true:
+                        operand1 = displayValue;
+                        operator = button.value;
+                        break;
+                    case false:
+                        operand2 = displayValue
+                        displayValue = operate(operand1, operand2, operator);
+                        updateDisplay();
+                        operand1 = displayValue;
+                        operator = button.value;
+                        break;
+                }
+            }
+            if(button.value === "="){
+                if(operand1){
+                    operand2 = displayValue;
+                    displayValue = operate(operand1, operand2, operator);
+                    updateDisplay();
+                    resetValues();
+                }
+            }
+        }
+        if(button.classList.contains("special")){
+            if(button.value === "AC"){
+                resetValues();
+                updateDisplay();
+            }
+        }
     })
 })
+
+function updateDisplay(){
+    display.textContent = displayValue;
+    if(displayValue.length > 9) {
+        display.textContent = displayValue.substring(0, 9);
+    }
+}
+
+function resetValues(){
+    displayValue = 0;
+    operand1 = null;
+    operand2 = null;
+    operator = null;
+}
 
 
 function operate(num1, num2, operator){
@@ -24,16 +83,9 @@ function operate(num1, num2, operator){
     }
 }
 
-let updateDisplay = displayValue => {
-    if(!isNaN(displayValue)){
-        display.textContent == 0 ? display.textContent = displayValue 
-                                 : display.textContent += displayValue;
-    }
-}
-
 //Operations
 let divide = (num1, num2) => num1/num2;
 let subtract = (num1, num2) => num1-num2;
 let multiply = (num1, num2) => num1 * num2;
-let add = (num1, num2) => num1 + num2;
+let add = (num1, num2) => (+num1) + (+num2);
 
